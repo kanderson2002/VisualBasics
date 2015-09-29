5ca41bb07fb231afe70c3c0da4ff018c11199acd
Kaitlyn A. <kaitlynanderson2002@gmail.com>
2015-09-29 14:40:59 -0700

    Create iFElse.vb

---
+++ iFElse.vb
@@ -0,0 +1,120 @@
+Module Module1
+
+    Sub Main()
+
+        Console.Write("Choose a number between numbers 1-25. Please type your answer and press Enter:")
+        Dim userValue As String
+
+        userValue = Console.ReadLine()
+        Console.WriteLine("You typed: " & userValue)
+
+        Console.WriteLine("Let's see what you got!")
+
+        If userValue = "1" Then
+            Console.WriteLine("You got a Baloon!")
+            Console.ReadLine()
+
+        ElseIf userValue = "2" Then
+            Console.WriteLine("You got a Iphone 6s!")
+            Console.ReadLine()
+
+        ElseIf userValue = "3" Then
+            Console.WriteLine("You got a $500 Gift Card To Barns 'N' Noble!")
+            Console.ReadLine()
+
+        ElseIf userValue = "4" Then
+            Console.WriteLine("You got a 6' Tall Tedy Bear!")
+            Console.ReadLine()
+
+        ElseIf userValue = "5" Then
+            Console.WriteLine("You got a Apple Laptop!")
+            Console.ReadLine()
+
+        ElseIf userValue = "6" Then
+            Console.WriteLine("You got a Bag Of Candy!")
+            Console.ReadLine()
+
+        ElseIf userValue = "7" Then
+            Console.WriteLine("You got a Bag Of Poo!")
+            Console.ReadLine()
+
+        ElseIf userValue = "8" Then
+            Console.WriteLine("You got a Purrari!")
+            Console.ReadLine()
+
+        ElseIf userValue = "9" Then
+            Console.WriteLine("You got a Snickers!")
+            Console.ReadLine()
+
+        ElseIf userValue = "10" Then
+            Console.WriteLine("You got a Jar Of Jelly")
+            Console.ReadLine()
+
+        ElseIf userValue = "11" Then
+            Console.WriteLine("You got a Tiger!")
+            Console.ReadLine()
+
+        ElseIf userValue = "12" Then
+            Console.WriteLine("You got a Wolf!")
+            Console.ReadLine()
+
+        ElseIf userValue = "13" Then
+            Console.WriteLine("You got a Frog!")
+            Console.ReadLine()
+
+        ElseIf userValue = "14" Then
+            Console.WriteLine("You got a Panda!")
+            Console.ReadLine()
+
+        ElseIf userValue = "15" Then
+            Console.WriteLine("You got a Nyann Cat!")
+            Console.ReadLine()
+
+        ElseIf userValue = "16" Then
+            Console.WriteLine("You got a PopTart!")
+            Console.ReadLine()
+
+        ElseIf userValue = "17" Then
+            Console.WriteLine("You got a Disney Land Pass!")
+            Console.ReadLine()
+
+        ElseIf userValue = "18" Then
+            Console.WriteLine("You got a Pig!")
+            Console.ReadLine()
+
+        ElseIf userValue = "19" Then
+            Console.WriteLine("You got a Gold Horse!")
+            Console.ReadLine()
+
+        ElseIf userValue = "20" Then
+            Console.WriteLine("You got a Box Of Irratated Hornets!")
+            Console.ReadLine()
+
+        ElseIf userValue = "21" Then
+            Console.WriteLine("You got a Container Full of Coconuts!")
+            Console.ReadLine()
+
+        ElseIf userValue = "22" Then
+            Console.WriteLine("You got a Bunny!")
+            Console.ReadLine()
+
+        ElseIf userValue = "23" Then
+            Console.WriteLine("You got a 2 Gerbals!")
+            Console.ReadLine()
+
+        ElseIf userValue = "24" Then
+            Console.WriteLine("You got a Deathstalker!")
+            Console.ReadLine()
+
+        ElseIf userValue = "25" Then
+            Console.WriteLine("You got a Black Mamba!")
+            Console.ReadLine()
+
+        Else
+            Console.WriteLine("Sorry I cannot comprehend your answer. YOU LOSE!")
+
+        End If
+
+    End Sub
+
+End Module
